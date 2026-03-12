b717253ac020e23a49de5f94d958ddf4edd60722
Chennamsetti-Harini <harinich966@gmail.com>
2026-03-12 17:02:00 +0530

    Fix RevHireApplicationTests - exclude DataSource for context load test

--- src/test/java/org/example/revhire/RevHireApplicationTests.java
+++ src/test/java/org/example/revhire/RevHireApplicationTests.java
@@ -2,12 +2,21 @@ package org.example.revhire;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.mail.host=localhost",
+        "spring.mail.port=25"
+})
 class RevHireApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
